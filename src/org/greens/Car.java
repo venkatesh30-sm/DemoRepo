dd8ef02788f8da47008bd69b442d7669637ad1e3
venkatesh3005 <venkateshv6868@gmail.com>
2020-01-21 13:08:06 +0530

    project added

---
+++ src/org/greens/Car.java
@@ -0,0 +1,21 @@
+package org.greens;
+
+public class Car {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		String s="123,12";
+		int i = Integer.parseInt(s);
+		System.out.println(i);
+		
+		
+	}
+	
+	
+	
+	
+	
+
+}
